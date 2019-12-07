0b1b9d3047b62e8e53f1ed65f271fdae1ce81e82
andronix01 <andronix01@gmail.com>
2019-12-07 20:46:23 +0800

    Add Pesbuk apps

---
+++ config-prd.php
@@ -0,0 +1,14 @@
+<?php
+
+$db_host = "dbhexa.c3vzduom9bjz.us-east-1.rds.amazonaws.com";
+$db_user = "devopscilsy";
+$db_pass = "1234567890";
+$db_name = "dbsosmed";
+
+try {
+    //create PDO connection
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
